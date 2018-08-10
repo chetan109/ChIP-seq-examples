492876a6e41af00e026dacd912783f6411e594fe
rochevin <vincent.rocher.armd@gmail.com>
2018-08-10 14:25:00 +0200

    Split code into two R scripts

--- scriptR/get_asi_location.R
+++ scriptR/get_asi_location.R
@@ -1,6 +1,7 @@
-### On charge la library pour h19
+### packages
 library("BSgenome.Hsapiens.UCSC.hg19")
-
+library("rtracklayer")
+library("dplyr")
 #### On assigne le genome à une variable
 hg19 = BSgenome.Hsapiens.UCSC.hg19
 
@@ -14,21 +15,17 @@ chromosomes = seqnames(hg19)
 
 file = "ASIsites_hg19.bed"
 
-full_data = data.frame()
+asi = data.frame()
 
 for( chromosome in paste0("chr",c(1:22,"X")) ) {
     chr_hg19 = hg19[[chromosome]]
     cc = matchPattern(ASI, chr_hg19, max.mismatch=0)
     taille = length(cc)
     chr_names = rep(chromosome,taille)
-    data = data.frame(chromosome_name=chr_names,start=start(cc),end=end(cc),check.names=FALSE)
-    full_data = rbind(full_data,data)
+    data = data.frame(seqnames=chr_names,start=start(cc),end=end(cc),check.names=FALSE)
+    asi = rbind(asi,data)
 }
 
 
-#Dans l'ancien jeu de données, le site 33 est une duplication du site 35, pour garder les mêmes noms, on passe de SITE32 à SITE34 directement
-names = c(paste("SITE",1:32,sep=""),paste("SITE",34:1212,sep=""))
-
-full_data = cbind(full_data,names)
-
-write.table(full_data, file=file, quote=FALSE, sep="\t",row.names=FALSE, col.names=FALSE)
+asi <- asi%>% GRanges()
+export.bed(asi,file)
---
+++ scriptR/get_repair_classes.R
@@ -0,0 +1,58 @@
+### packages
+library("BSgenome.Hsapiens.UCSC.hg19")
+library("rtracklayer")
+library("dplyr")
+#Compute classes HR/NHEJ based on RAD51 and XRCC4 dataset (Aymard et al, 2014. E-MTAB-1241)
+bless80 <- import.bed("/mnt/volumes/NAS1/DATA/AsiSI/BLESS_80best_JunFragPE_Rmdups_pm500bp.bed")
+bw.files <- c("/mnt/volumes/NAS1/DATA/ChIP-Seq/RAD51/U2OS/EMBL2/PROCESSED/WIGGLE/hg19/RAD51_hg19_normalized.bw",
+              "/mnt/volumes/NAS1/DATA/ChIP-Seq/XRCC4/U2OS/EMBL2/PROCESSED/WIGGLE/hg19/XRCC4_hg19_normalized.bw",
+              "/mnt/volumes/NAS1/DATA/ChIP-Seq/HISTONES/Clouaire_HVL7VBGX5/PROCESSED/mapping/BIGWIG/HVL7VBGX5_Lig4_2_OHT_18s000869-1-1_Clouaire_lane118s000869_sequence_normalized.bw")
+names(bw.files) <- c("RAD51","XRCC4","Lig4")
+windows <- c(2000,250,500)
+names(windows) <- names(bw.files)
+
+cat.dat <- NULL
+for(n in names(bw.files)){
+    wig <- import.bw(bw.files[n],as = "RleList")
+    my.w <- windows[n]
+    count <- compute1ValPerSiteForBLESS(bed=bless80,wig=wig,w=my.w,seqlens = seqlens,fun="mean")
+    if(is.null(cat.dat)){
+        cat.dat <- data.frame("Value"=count)
+        colnames(cat.dat) <- n
+    }else{
+        oldcol <- colnames(cat.dat)
+        cat.dat <- cbind(cat.dat,count)
+        colnames(cat.dat) <- c(oldcol,n)
+    }
+}
+
+#Compute ratio for each
+cat.dat <- cat.dat %>%
+    mutate(Ratio_RAD51.XRCC4 = (RAD51+0.01)/(XRCC4+0.01)) %>%
+    mutate(Ratio_RAD51.Lig4 = (RAD51+0.01)/(Lig4+0.01))
+
+#Extract classes for RAD51/XRCC4 ratio
+ratio.pos <- cat.dat %>% mutate(pos = row_number())%>% arrange(desc(Ratio_RAD51.XRCC4))
+#Extract HR sites
+HR.sites <- ratio.pos  %>% dplyr::slice(1:30) %>% pull(pos)
+HR.sites <- bless80[HR.sites] %>% sortSeqlevels() %>% sort()
+
+export.bed(HR.sites,"BLESS_HR_JunFragPE_Rmdups_pm500bp.bed")
+#Extract NHEJ sites
+NHEJ.sites <- ratio.pos  %>% dplyr::slice(51:80) %>% pull(pos)
+NHEJ.sites <- bless80[NHEJ.sites] %>% sortSeqlevels() %>% sort()
+
+export.bed(NHEJ.sites,"BLESS_NHEJ_JunFragPE_Rmdups_pm500bp.bed")
+
+#Extract classes for RAD51/XRCC4 ratio
+ratio.pos <- cat.dat %>% mutate(pos = row_number())%>% arrange(desc(Ratio_RAD51.Lig4))
+#Extract HR sites
+HR.sites <- ratio.pos  %>% dplyr::slice(1:30) %>% pull(pos)
+HR.sites <- bless80[HR.sites] %>% sortSeqlevels() %>% sort()
+
+export.bed(HR.sites,"BLESS_HR_JunFragPE_Rmdups_pm500bp_Lig4.bed")
+#Extract NHEJ sites
+NHEJ.sites <- ratio.pos  %>% dplyr::slice(51:80) %>% pull(pos)
+NHEJ.sites <- bless80[NHEJ.sites] %>% sortSeqlevels() %>% sort()
+
+export.bed(NHEJ.sites,"BLESS_NHEJ_JunFragPE_Rmdups_pm500bp_Lig4.bed")
